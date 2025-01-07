0dbf44e4250ad9a669859da4c23744efd7aa0d19
polarsama <andreska9506@gmail.com>
2025-01-06 21:25:27 -0500

    update

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,15 +6,19 @@ public class Player : MonoBehaviour
 {
     public float speed = 0;
     public float speedRotation = 0;
+    public Vector2 sensibility;
 
     public Animator animator;
 
     private float x, y;
+    private new Transform camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+
+        camera = transform.Find("MainCamera");
     }
 
     // Update is called once per frame
@@ -29,6 +33,26 @@ public class Player : MonoBehaviour
 
         animator.SetFloat("VelX", x);
         animator.SetFloat("VelY", y);
+
+
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+
+        if (hor != 0)
+        {
+            transform.Rotate(Vector3.up * hor * sensibility);
+        }
+
+        if (ver != 0)
+        {
+            float angle = (camera.localEulerAngles.x - ver * sensibility.y + 360) % 360;
+
+            if (angle > 180) { angle -= 360; }
+            angle = Mathf.Clamp(angle, -80, 80);
+
+            camera.localEulerAngles = Vector3.right * angle;
+        }
+
 
     }
 }
