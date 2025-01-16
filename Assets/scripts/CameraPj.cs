f98ee02775f7a0a39e6eae9a56b628b2d8bc5278
polarsama <andreska9506@gmail.com>
2025-01-15 21:31:48 -0500

    update

--- Assets/scripts/CameraPj.cs
+++ Assets/scripts/CameraPj.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CameraPj : MonoBehaviour
 {
+
+    public Vector2 sensibility;
+
+    private new Transform camera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+
+        camera = transform.Find("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float hor = Input.GetAxis("Mouse X");
+        float ver = Input.GetAxis("Mouse Y");
+
+        if (hor != 0)
+        {
+            transform.Rotate(Vector3.up * hor * sensibility);
+        }
+
+        if (ver != 0)
+        {
+            float angle = (camera.localEulerAngles.x - ver * sensibility.y + 360) % 360;
+
+            if (angle > 180) { angle -= 360; }
+            angle = Mathf.Clamp(angle, -80, 80);
+
+            camera.localEulerAngles = Vector3.right * angle;
+        }
     }
 }
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,23 +6,23 @@ public class Player : MonoBehaviour
 {
     private Rigidbody Rgb;
 
-    public CharacterController controller;
+    //public CharacterController controller;
 
     public float movementSpeed = 0;
 
-    public Vector2 sensibility;
+    //public Vector2 sensibility;
 
-    public Animator animator;
+    //public Animator animator;
 
     private float x, z, y;
-    private new Transform camera;
+    //private new Transform camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
 
-        camera = transform.Find("MainCamera");
+        //camera = transform.Find("MainCamera");
 
         Rgb = GetComponent<Rigidbody>();
     }
@@ -35,30 +35,30 @@ public class Player : MonoBehaviour
 
         Vector3 move = (transform.forward * z + transform.right * x).normalized;
 
-        controller.Move(move);
+        //controller.Move(move);
 
 
-        animator.SetFloat("VelX", x);
-        animator.SetFloat("VelY", y);
+        //animator.SetFloat("VelX", x);
+        //animator.SetFloat("VelY", y);
 
 
-        float hor = Input.GetAxis("Mouse X");
-        float ver = Input.GetAxis("Mouse Y");
+        //float hor = Input.GetAxis("Mouse X");
+        //float ver = Input.GetAxis("Mouse Y");
 
-        if (hor != 0)
-        {
-            transform.Rotate(Vector3.up * hor * sensibility);
-        }
+        //if (hor != 0)
+        //{
+        //    transform.Rotate(Vector3.up * hor * sensibility);
+        //}
 
-        if (ver != 0)
-        {
-            float angle = (camera.localEulerAngles.x - ver * sensibility.y + 360) % 360;
+        //if (ver != 0)
+        //{
+        //    float angle = (camera.localEulerAngles.x - ver * sensibility.y + 360) % 360;
 
-            if (angle > 180) { angle -= 360; }
-            angle = Mathf.Clamp(angle, -80, 80);
+        //    if (angle > 180) { angle -= 360; }
+        //    angle = Mathf.Clamp(angle, -80, 80);
 
-            camera.localEulerAngles = Vector3.right * angle;
-        }
+        //    camera.localEulerAngles = Vector3.right * angle;
+        //}
 
 
     }
